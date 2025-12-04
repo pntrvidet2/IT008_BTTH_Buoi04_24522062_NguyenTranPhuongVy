5c4a023f91b3fda3b1cc53028862bc223d05104f
24522062_NguyenTranPhuongVy <24522062@gm.uit.edu.vn>
2025-12-04 22:24:38 +0700

    Add files via upload

---
+++ BT01/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Media Files|*.mp3;*.mp4;*.avi;*.mpeg;*.wav;*.midi";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = dlg.FileName;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ứng dụng sẽ đóng. Cảm ơn bạn đã sử dụng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = $"Hôm nay là ngày {DateTime.Now:dd/MM/yyyy} - Bây giờ là {DateTime.Now:HH:mm:ss tt}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
