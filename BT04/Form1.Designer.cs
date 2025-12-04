7e9dedfe07dbe438670f00d0aa538ccc19343af5
24522062_NguyenTranPhuongVy <24522062@gm.uit.edu.vn>
2025-12-04 22:28:30 +0700

    Add files via upload

---
+++ BT04/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace BT04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.btnBrowseDest = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCopy);
+            this.groupBox1.Controls.Add(this.btnBrowseSource);
+            this.groupBox1.Controls.Add(this.btnBrowseDest);
+            this.groupBox1.Controls.Add(this.txtDest);
+            this.groupBox1.Controls.Add(this.txtSource);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 235);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sao chép tập tin";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCopy.ForeColor = System.Drawing.Color.Black;
+            this.btnCopy.Location = new System.Drawing.Point(230, 161);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(158, 35);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Sao Chép";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBrowseSource.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseSource.ForeColor = System.Drawing.Color.Blue;
+            this.btnBrowseSource.Location = new System.Drawing.Point(683, 65);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(41, 23);
+            this.btnBrowseSource.TabIndex = 7;
+            this.btnBrowseSource.Text = "...";
+            this.btnBrowseSource.UseVisualStyleBackColor = false;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // btnBrowseDest
+            // 
+            this.btnBrowseDest.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBrowseDest.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseDest.ForeColor = System.Drawing.Color.Blue;
+            this.btnBrowseDest.Location = new System.Drawing.Point(683, 103);
+            this.btnBrowseDest.Name = "btnBrowseDest";
+            this.btnBrowseDest.Size = new System.Drawing.Size(41, 23);
+            this.btnBrowseDest.TabIndex = 6;
+            this.btnBrowseDest.Text = "...";
+            this.btnBrowseDest.UseVisualStyleBackColor = false;
+            this.btnBrowseDest.Click += new System.EventHandler(this.btnBrowseDest_Click);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(281, 104);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.ReadOnly = true;
+            this.txtDest.Size = new System.Drawing.Size(396, 22);
+            this.txtDest.TabIndex = 5;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(281, 66);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(396, 22);
+            this.txtSource.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(53, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Đường Dẫn Thư Mục Nguồn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(53, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Đường Dẫn Thư Mục Đích";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(12, 253);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tiến trình sao chép";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 35);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(737, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 376);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(101, 16);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Đang sao chép:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sao chép tập tin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnBrowseSource;
+        private System.Windows.Forms.Button btnBrowseDest;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ BT04/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // ToolTip cho các control
+            toolTip1.SetToolTip(btnBrowseSource, "Chọn file nguồn để sao chép");
+            toolTip1.SetToolTip(txtDest, "Chọn thư mục đích");
+            toolTip1.SetToolTip(btnCopy, "Nhấn để sao chép file");
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+        }
+
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Chọn file nguồn";
+            ofd.Filter = "Tất cả tập tin|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+                txtSource.Text = ofd.FileName;
+        }
+
+        private void btnBrowseDest_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Chọn thư mục đích";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                txtDest.Text = fbd.SelectedPath;
+        }
+
+        private async void btnCopy_Click(object sender, EventArgs e)
+        {
+            string src = txtSource.Text;
+            string destFolder = txtDest.Text;
+
+            if (!File.Exists(src))
+            {
+                MessageBox.Show("File nguồn không tồn tại!");
+                return;
+            }
+            if (!Directory.Exists(destFolder))
+            {
+                MessageBox.Show("Thư mục đích không tồn tại!");
+                return;
+            }
+
+            string fileName = Path.GetFileName(src);
+            string dest = Path.Combine(destFolder, fileName);
+
+            progressBar1.Value = 0;
+            lblStatus.Text = "Đang sao chép: " + fileName;
+            toolTip1.SetToolTip(progressBar1, "Đang sao chép...");
+
+            await CopyFileAsync(src, dest);
+
+            progressBar1.Value = 100;
+            lblStatus.Text = "Hoàn thành!";
+            toolTip1.SetToolTip(progressBar1, "Hoàn tất sao chép");
+            MessageBox.Show("Sao chép thành công!");
+        }
+
+        private async Task CopyFileAsync(string source, string dest)
+        {
+            const int bufferSize = 1024 * 1024; // 1MB
+            byte[] buffer = new byte[bufferSize];
+            string fileName = Path.GetFileName(source);
+
+            using (FileStream sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read))
+            using (FileStream destStream = new FileStream(dest, FileMode.Create))
+            {
+                long totalBytes = sourceStream.Length;
+                long totalCopied = 0;
+
+                int bytesRead;
+                while ((bytesRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    await destStream.WriteAsync(buffer, 0, bytesRead);
+                    totalCopied += bytesRead;
+
+                    int percent = (int)(totalCopied * 100 / totalBytes);
+                    if (percent > 100) percent = 100;
+
+                    progressBar1.Value = percent;
+                    lblStatus.Text = $"Đang sao chép: {fileName} - {percent}%";
+
+                    toolTip1.SetToolTip(progressBar1, lblStatus.Text);
+
+                    await Task.Delay(50); // để progress bar đi từ từ
+                }
+            }
+        }
+
+    }
+}
